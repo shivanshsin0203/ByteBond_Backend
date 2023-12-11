de7eac4fa7a74ba527e8ed553dee49c0a9ad7eb7
Shivansh Singh <singhshivansh12may@gmail.com>
2023-12-11 11:27:18 +0530

    Internal Adjustments

--- index.js
+++
@@ -1,9 +0,0 @@
-const express=require('express')
-const {port}=require('./src/config/serverconfig')
-const app=express();
-const connect=require('./src/config/database');
-app.listen(port,async ()=>{
-      console.log("Server Started at "+port);
-      await connect();
-      console.log("Connected to databse");
-     })
