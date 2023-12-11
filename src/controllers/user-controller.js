e4715e338dca8a30976267ff04a9388600838b86
Shivansh Singh <singhshivansh12may@gmail.com>
2023-12-11 15:48:22 +0530

    Restricted dublicates

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -3,9 +3,7 @@ const userserv=new userservice();
 
 const adduser=async(req,res)=>{
     try{
-        console.log(req.body);
         const result=await userserv.adduser(req.body);
-        
         res.status(200).json(
             {   data:result,
                 message:"Signed in successfully",
