3fb505ead522bc7ca2c06c81e9075dd544fe4e3f
Shivansh Singh <singhshivansh12may@gmail.com>
2023-12-18 10:43:21 +0530

    Added Recommendation system

--- src/services/recommend.js
+++ src/services/recommend.js
@@ -7,7 +7,18 @@ class RecommendService{
         const user=await this.userrepo.getuserbyid(id);
         const data=await this.userrepo.getallusers();
         const removeddata=data.filter(element=>element.email!==user.email);
-        
+        const recommend=removeddata.map((element)=>{
+            const match=element.skills.filter(skill=>user.skills.includes(skill));
+            let score=match.length;
+            if(element.college===user.college){
+                score=score+2;
+            }
+            
+            return { ...element._doc, score: score };
+        });
+        recommend.sort((a,b)=>b.score-a.score);
+        return recommend;
+      
     }
 }
 module.exports=RecommendService;
