cb7f933edb5c9306483a9720c3015b8892b8a8d2
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-01 21:36:38 +0530

    Restructuring folders

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
-dotenv.config({path:'../.env'});
+dotenv.config({path:'/.env'});
 const mongoUrl=process.env.mongoUrl
 const coonect=async()=>{
     await mongoose.connect(mongoUrl)
--- config/serverconfig.js
+++ config/serverconfig.js
@@ -1,5 +1,5 @@
 const dotenv=require('dotenv');
-dotenv.config({path:'../.env'});
+dotenv.config({path:'./.env'});
 module.exports={
     port:process.env.port,
     Email_Id:process.env.Email_Id,
